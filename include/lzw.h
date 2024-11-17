c5b3d9ea0561754f273f6f2249fa5d3728ae232f
x0r4ks <max1500demo@gmail.com>
2024-11-17 14:25:19 +0300

    add vector type

--- include/lzw.h
+++ include/lzw.h
@@ -7,10 +7,69 @@
 
 namespace lzw
 {
+    template<typename T>
+    std::vector<T> compress(const std::string& input) {
+        // Initialize
+        std::vector<int> result;
+        std::unordered_map<std::string, int> dict;
 
-    std::vector<int> compress(const std::string& input);
-    
-    std::string decompress(const std::vector<int>& input);
+        for (int c = 0; c < 256; c++) {
+            dict[std::string(1,c)] = c;
+        }
+
+        //Compression
+        std::string current = "";
+        
+        for (int i = 0; i < input.size(); i++) {
+            if (dict.count(current + input[i])) {
+                current += input[i];
+                
+            } else {
+                result.push_back(dict[current]);
+                dict[current + input[i]] = dict.size();
+                current = input[i];
+            }
+        }
+        if (!current.empty()) {
+            result.push_back(dict[current]);
+        }
+
+        return result;
+    }
+
+    template<typename T>
+    std::string decompress(const std::vector<T>& input) {
+        // Initialize
+        std::string result = "";
+        std::unordered_map<int, std::string> dict;
+        for (int i = 0 ; i < 256; i++) {
+            dict[i] = std::string(1,char(i));
+            
+        }
+
+        //Decompression
+        std::string w(1, char(input[0]));
+        result += w;
+
+        for (int i = 1; i < input.size(); ++i) {
+            int k = input[i];
+            std::string entry;
+
+            if (dict.count(k)) {
+                entry = dict[k];
+            }
+            else  if (k == dict.size()){
+                entry = w + w[0];
+            }
+
+            result += entry;
+            dict[dict.size()] = w + entry[0];
+            w = entry;
+        }
+
+
+        return result;
+    }
 
 } // namespace lzw
 
